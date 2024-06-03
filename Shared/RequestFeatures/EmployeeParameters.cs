c122d7b756e539ff44f5153d6489c286329be5fa
Olawale Akin-Odanye <olawale.akinodanye@gmail.com>
2024-06-04 00:13:56 +0100

    Added filtering

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Shared.RequestFeatures
 {
     public class EmployeeParameters : RequestParameters
     {
-        
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = uint.MaxValue;
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
